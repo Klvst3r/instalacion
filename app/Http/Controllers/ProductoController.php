cb8aac8d1062a331fc133b18205ffcbac97330ee
klvst3r <klvst3r@gmail.com>
2018-04-30 18:39:51 -0500

    First save with eloquent

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Producto; //importa
 use App\Http\Requests;
 
 class ProductoController extends Controller
@@ -11,4 +12,21 @@ class ProductoController extends Controller
     public function getCrear(){
     	return view("producto.crear");
     }
+
+    public function postCrearnuevo(Request $request){
+    	//return $request->all();
+    	
+    	$producto = new Producto();
+    	$producto->nombre = $request->input("nombre");
+    	$producto->detalle = $request->input("detalle");
+    	$producto->precio = $request->input("precio");
+    	$producto->cantidad = $request->input("cantidad");
+
+    	if($producto->save()){
+    		return "ok";
+    	}
+    	 
+
+    }
+    	
 }
--- resources/views/producto/crear.blade.php
+++ resources/views/producto/crear.blade.php
@@ -3,17 +3,31 @@
 @section("contenido")
 		
 	<div class="col-md-4">
-		<form action="" method="POST" role="form">
+		<form action="{{ url('producto/crearnuevo') }}" method="POST" role="form">
+			
+			{{ csrf_field() }}
+
 			<legend>Crear Producto</legend>
 		
 			<div class="form-group">
-				<label for="">label</label>
-				<input type="text" class="form-control" id="" placeholder="Input field">
+				<label for="">Nombre</label>
+				<input type="text" class="form-control" name="nombre" id="" placeholder="Input field">
+			</div>
+			<div class="form-group">
+				<label for="">Detalle</label>
+				<input type="text" class="form-control" name="detalle" id="" placeholder="Input field">
+			</div>
+			<div class="form-group">
+				<label for="">Precio</label>
+				<input type="text" class="form-control" name="precio" id="" placeholder="Input field">
+			</div>
+			<div class="form-group">
+				<label for="">Cantidad</label>
+				<input type="text" class="form-control" name="cantidad" id="" placeholder="Input field">
 			</div>
 		
-			
 		
-			<button type="submit" class="btn btn-primary">Submit</button>
+			<button type="submit" class="btn btn-primary">Enviar</button>
 		</form>
 	</div>
 
